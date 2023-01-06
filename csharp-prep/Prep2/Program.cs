881b621ac5b881044d1d341807131cd882dff509
luiscancinourcid <luiscancinourcid@gmail.com>
2023-01-06 14:50:26 -0800

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letter = "";
+        int remainder = 0;
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            letter = ("A");
+        }
+        else if (percentage >= 80)
+        {
+            letter = ("B");
+        }
+        else if (percentage >= 70)
+        {
+            letter = ("C");
+        }
+        else if (percentage >= 60)
+        {
+            letter = ("D");
+        }
+        else
+        {
+            letter = ("F");
+        }
+
+        
+        remainder = percentage % 10;
+
+        
+        
+        if (remainder >= 7)
+        {
+            sign = "+";
+        }
+        else
+        {
+            sign = "-";
+        }
+        
+
+        if (letter == "A" || letter == "F" )
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}{sign}");
+        }
+        if (percentage >= 60)
+        {
+            Console.Write("You passed!!");
+        }
+        else
+        {
+            Console.Write("Sorry, try again the next time.");
+        }
+
     }
 }
