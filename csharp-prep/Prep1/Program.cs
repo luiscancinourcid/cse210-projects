678dc5cdeb630288c3d36ea894c4c6a2262f93d1
luiscancinourcid <luiscancinourcid@gmail.com>
2023-01-06 13:33:39 -0800

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What's your fist name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
